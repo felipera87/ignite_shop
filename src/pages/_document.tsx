8b65ab9e6ba0bbd596f04ddefa2f1659c8319049
felipera8732 <felipe@pruvo.com>
2023-04-05 09:40:53 -0300

    Added Stitches and a simple use case

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,10 @@
+import { getCssText } from '@/styles'
 import { Html, Head, Main, NextScript } from 'next/document'
+
+/**
+ * dangerouslySetInnerHTML allows the css to render on server side instead of loading using javascript code on browser.
+ * If user disable javascript it'll not load the css, but with that style tag it process the css on server
+ */
 
 export default function Document() {
   return (
@@ -7,6 +13,8 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </Head>
       <body>
         <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
+import { styled } from "@/styles"
+
+const Button = styled('button', {
+  backgroundColor: '$testColor',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold'
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  },
+})
+
 export default function Home() {
   return (
-    <h1>hello</h1>
+    <Button>
+      <span>test</span>
+      enviar
+    </Button>
   )
 }
