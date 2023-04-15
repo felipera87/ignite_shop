453ec242d5d7bb3e3694c9cdac04942b15df5c83
felipera8732 <felipe@pruvo.com>
2023-04-15 10:31:02 -0300

    Added Success page with static values

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Felipe Reis</strong>, your <strong>Shirt Beyond the Limits</strong> is on the way.
+      </p>
+
+      <Link href="/">
+        Back to catalog
+      </Link>
+    </SuccessContainer>
   )
 }
